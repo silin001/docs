381e9587743ce60813d9271f9a79c6bbff34aa9c
sisi <862956812@11.com>
2023-08-11 23:46:37 +0800

    清除本地git 缓存

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-npm run build
+yarn build
 # npm run docs:build
 
 # 进入生成的文件夹
@@ -12,9 +11,9 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m '手动部署'
 
 # 发布到gh-pages分支
-git push -f git@github.com:silin001/vuePress-blog.git master:gh-pages
+git push -f git@github.com:silin001/vuePress-blog.git main:gh-pages
 
 # cd ../ && rm -rf dist
