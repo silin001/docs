833eb211b037f3b1f663b8a52913d713adc26568
silin001 <862956812@qq.com>
2023-08-22 23:15:11 +0800

    mac测试

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,34 +1,32 @@
-
 // CommonJS 模块使用 require() 和 module.exports
 // ES6 模块使用 import 和 export 。
-import { defineUserConfig } from 'vuepress'
-import { recoTheme } from 'vuepress-theme-reco'
-// const { vuepressTheme }  = require('vuepress-theme-reco')
+import { defineUserConfig } from "vuepress";
+import { recoTheme } from "vuepress-theme-reco";
+// const { recoTheme } = require("vuepress-theme-reco");
 export default defineUserConfig({
   // module.exports = {
-  title: 's',
-  description: '尖椒土豆sss的博客',
+  title: "s11",
+  description: "尖椒土豆sss的博客",
   port: 3333,
-  base: '/docs/',
+  base: "/docs/",
   // publicPath: '/sl/',
   // 默认 /  https://<USERNAME or GROUP>.github.io/
   // 如果你打算发布到 https://<USERNAME or GROUP>.github.io/<REPO>/（也就是说你的仓库在 https://github.com/<USERNAME>/<REPO>），则将 base 设置为 "/<REPO>/"。
   // base: '/docs/',
   // base: '/',
   theme: recoTheme({
-    
     type: "blog",
     author: "尖椒土豆sss",
     logo: "/img/avatar.png",
     authorAvatar: "/img/avatar.png",
-    colorMode: 'dark', // dark, light
+    colorMode: "dark", // dark, light
     // 自动设置分类
     autoSetBlogCategories: true,
     // 自动将分类和标签添加至头部导航条
     autoAddCategoryToNavbar: {
       location: 1, // 默认 0
-      categoryText: '文章分类', // 默认 categories
-      tagText: '标签' // 默认 tags
+      categoryText: "文章分类", // 默认 categories
+      tagText: "标签", // 默认 tags
     },
     // 自动设置系列
     // autoSetSeries: true,
@@ -38,24 +36,24 @@ export default defineUserConfig({
     // 导航栏配置
     navbar: [
       {
-        text: '首页',
-        link: '/'
+        text: "首页",
+        link: "/",
       },
       {
-        text: '工具链接',
-        icon: 'Link',
+        text: "工具链接",
+        icon: "Link",
         children: [
-          { text: '工具箱', link: 'http://tool.mkblog.cn/' },
-          { text: '百度一下', link: 'https://www.baidu.com/' },
-        ]
+          { text: "工具箱", link: "http://tool.mkblog.cn/" },
+          { text: "百度一下", link: "https://www.baidu.com/" },
+        ],
       },
       {
-        text: '官方文档',
-        icon: 'Document',
+        text: "官方文档",
+        icon: "Document",
         children: [
-          { text: 'vue2官网', link: 'https://v2.cn.vuejs.org/' },
-          { text: 'vue3官网', link: 'https://cn.vuejs.org/' },
-        ]
+          { text: "vue2官网", link: "https://v2.cn.vuejs.org/" },
+          { text: "vue3官网", link: "https://cn.vuejs.org/" },
+        ],
       },
       // {
       //   text: 'Vue相关',
@@ -82,20 +80,20 @@ export default defineUserConfig({
       //   ]
       // },
       {
-        text: '关于我',
-        icon: 'UserAvatarFilledAlt',
+        text: "关于我",
+        icon: "UserAvatarFilledAlt",
         children: [
           {
-            text: '掘金',
-            link: 'https://juejin.cn/user/2041110775208184/posts'
+            text: "掘金",
+            link: "https://juejin.cn/user/2041110775208184/posts",
           },
           // {
           //   text: 'GitHub',
           //   link: 'https://github.com/silin001'
           // },
-        ]
+        ],
       },
     ],
-  })
+  }),
   // }
-})
+});
