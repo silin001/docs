3177813279aac8916b77c0233bac91b272b4e51f
sisi <862956812@qq.com>
2023-08-23 13:35:38 +0800

    cs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,26 +1,15 @@
 // CommonJS 模块使用 require() 和 module.exports
 // ES6 模块使用 import 和 export 。
 console.log('ts config')
-import { defineUserConfig } from "vuepress";
 import { recoTheme } from "vuepress-theme-reco";
-export default defineUserConfig({
-// module.exports = {
-  title: "sts1",
-  // description: "尖椒土豆sss的博客",
+module.exports = {
+  title: "1111js",
   port: 3333,
   base: "/docs/",
-  // publicPath: '/sl/',
-  // 默认 /  https://<USERNAME or GROUP>.github.io/
-  // 如果你打算发布到 https://<USERNAME or GROUP>.github.io/<REPO>/（也就是说你的仓库在 https://github.com/<USERNAME>/<REPO>），则将 base 设置为 "/<REPO>/"。
-  // base: '/docs/',
-  // base: '/',
   theme: recoTheme({
     type: "blog",
     author: "尖椒土豆sss",
     logo: "/img/avatar.png",
-    // authorAvatar: "/img/avatar.png",
-    // colorMode: "dark", // dark, light
-    // colorMode: "light", // dark, light
     // 自动设置分类
     autoSetBlogCategories: true,
     // 自动将分类和标签添加至头部导航条
@@ -29,11 +18,6 @@ export default defineUserConfig({
       categoryText: "文章分类", // 默认 categories
       tagText: "标签", // 默认 tags
     },
-    // 自动设置系列
-    // autoSetSeries: true,
-    // 内置搜索
-    // search: true,
-    // searchMaxSuggestions: 10,
     // 导航栏配置
     navbar: [
       {
@@ -56,30 +40,6 @@ export default defineUserConfig({
           { text: "vue3官网", link: "https://cn.vuejs.org/" },
         ],
       },
-      // {
-      //   text: 'Vue相关',
-      //   icon: 'LogoVue',
-      //   children: [
-      //     {
-      //       text: 'vue2相关',
-      //       children: [
-      //         {
-      //           text: 'vue2.0相关实现原理记录',
-      //           link: '/blogs/vue2/vue2相关实现原理.md'
-      //         }
-      //       ]
-      //     },
-      //     // {
-      //     //   text: 'vue3相关',
-      //     //   children: [
-      //     //     {
-      //     //       text: 'vue3xxx',
-      //     //       link: '/vue/vue2/ym01'
-      //     //     }
-      //     //   ]
-      //     // }
-      //   ]
-      // },
       {
         text: "关于我",
         icon: "UserAvatarFilledAlt",
@@ -96,5 +56,4 @@ export default defineUserConfig({
       },
     ],
   }),
-  // }
-});
+  }
