c0732da87bbdb987ef1ff39fb293e0bfc1b4ce1a
sisi <862956812@qq.com>
2023-08-24 09:58:36 +0800

    测试一下

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,87 +1,53 @@
-
-import { defineUserConfig } from 'vuepress'
-import { recoTheme } from 'vuepress-theme-reco'
+import { defineUserConfig } from "vuepress";
+import { recoTheme } from "vuepress-theme-reco";
 export default defineUserConfig({
-  // 默认 /  https://<USERNAME or GROUP>.github.io/
-  // 如果你打算发布到 https://<USERNAME or GROUP>.github.io/<REPO>/（也就是说你的仓库在 https://github.com/<USERNAME>/<REPO>），则将 base 设置为 "/<REPO>/"。
-  title: "sss.ts",
+  title: "sss",
   port: 3333,
   base: "/docs/",
   theme: recoTheme({
     type: "blog",
     author: "尖椒土豆sss",
     logo: "/img/avatar.png",
-    authorAvatar: "/img/avatar.png",
     // 自动设置分类
     autoSetBlogCategories: true,
     // 自动将分类和标签添加至头部导航条
     autoAddCategoryToNavbar: {
       location: 1, // 默认 0
-      categoryText: '文章分类', // 默认 categories
-      tagText: '标签' // 默认 tags
+      categoryText: "文章分类", // 默认 categories
+      tagText: "标签", // 默认 tags
     },
-    // 自动设置系列
-    autoSetSeries: true,
     // 导航栏配置
     navbar: [
       {
-        text: '首页',
-        link: '/'
+        text: "首页",
+        link: "/",
       },
       {
-        text: '工具链接',
-        icon: 'Link',
+        text: "工具链接",
+        icon: "Link",
         children: [
-          { text: '工具箱', link: 'http://tool.mkblog.cn/' },
-          { text: '百度一下', link: 'https://www.baidu.com/' },
-        ]
+          { text: "工具箱", link: "http://tool.mkblog.cn/" },
+          { text: "百度一下", link: "https://www.baidu.com/" },
+        ],
       },
       {
-        text: '官方文档',
-        icon: 'Document',
+        text: "官方文档",
+        icon: "Document",
         children: [
-          { text: 'vue2官网', link: 'https://v2.cn.vuejs.org/' },
-          { text: 'vue3官网', link: 'https://cn.vuejs.org/' },
-        ]
+          { text: "vue2官网", link: "https://v2.cn.vuejs.org/" },
+          { text: "vue3官网", link: "https://cn.vuejs.org/" },
+        ],
       },
-      // {
-      //   text: 'Vue相关',
-      //   icon: 'LogoVue',
-      //   children: [
-      //     {
-      //       text: 'vue2相关',
-      //       children: [
-      //         {
-      //           text: 'vue2.0相关实现原理记录',
-      //           link: '/blogs/vue2/vue2相关实现原理.md'
-      //         }
-      //       ]
-      //     },
-      //     // {
-      //     //   text: 'vue3相关',
-      //     //   children: [
-      //     //     {
-      //     //       text: 'vue3xxx',
-      //     //       link: '/vue/vue2/ym01'
-      //     //     }
-      //     //   ]
-      //     // }
-      //   ]
-      // },
       {
-        text: '关于我',
-        icon: 'UserAvatarFilledAlt',
+        text: "关于我",
+        icon: "UserAvatarFilledAlt",
         children: [
           {
-            text: '掘金',
-            link: 'https://juejin.cn/user/2041110775208184/posts'
+            text: "掘金",
+            link: "https://juejin.cn/user/2041110775208184/posts",
           },
-          // {
-          //   text: 'GitHub',
-          //   link: 'https://github.com/silin001'
-          // },
-        ]
+        ],
       },
     ],
-  })
-})
+  }),
+});
